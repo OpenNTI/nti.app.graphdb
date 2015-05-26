fe34226c7a4192b38ee3f0f707d83b4f6e0f2783
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-05-26 20:55:48 +0000

    initial checkin
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.graphdb/trunk@66029 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ setup.py
@@ -0,0 +1,38 @@
+import codecs
+from setuptools import setup, find_packages
+
+VERSION = '0.0.0'
+
+entry_points = {
+    "z3c.autoinclude.plugin": [
+		'target = nti.app',
+	],
+	'console_scripts': [
+	],
+}
+
+setup(
+	name='nti.app.graphdb',
+	version=VERSION,
+	author='Jason Madden',
+	author_email='jason.madden@nextthought.com',
+	description="NTI App GraphDB",
+	long_description=codecs.open('README.rst', encoding='utf-8').read(),
+	license='Proprietary',
+	keywords='pyramid preference',
+	classifiers=[
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.3',
+	],
+	packages=find_packages('src'),
+	package_dir={'': 'src'},
+	namespace_packages=['nti','nti.app'],
+	install_requires=[
+		'setuptools',
+		'nti.graphdb'
+	],
+	entry_points=entry_points
+)
---
+++ src/nti/app/graphdb/tests/test_admin_views.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from nti.app.testing.application_webtest import ApplicationLayerTest
+
+class TestAdminViews(ApplicationLayerTest):
+    pass
