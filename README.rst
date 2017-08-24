1693ce28c3eafeff7d70eab23b20c0ed623430b7
Carlos Sanchez <papachoco@gmail.com>
2017-08-24 16:14:26 -0500

    add supporting files

---
+++ README.rst
@@ -0,0 +1,9 @@
+=================
+ nti.app.graphdb
+=================
+
+.. image:: https://travis-ci.org/NextThought/nti.app.graphdb.svg?branch=master
+    :target: https://travis-ci.org/NextThought/nti.app.graphdb
+
+.. image:: https://coveralls.io/repos/github/NextThought/nti.app.graphdb/badge.svg?branch=master
+    :target: https://coveralls.io/github/NextThought/nti.app.graphdb?branch=master
--- setup.py
+++ setup.py
@@ -1,38 +1,59 @@
 import codecs
 from setuptools import setup, find_packages
 
-VERSION = '0.0.0'
-
 entry_points = {
-    "z3c.autoinclude.plugin": [
-		'target = nti.app',
-	],
-	'console_scripts': [
-	],
+    'z3c.autoinclude.plugin': [
+        'target = nti.app',
+    ],
+    'console_scripts': [
+        "nti_graph_constructor = nti.app.graphdb.constructor:main",
+    ],
 }
 
+
+TESTS_REQUIRE = [
+    'nti.app.testing',
+    'nti.testing',
+    'zope.testrunner',
+]
+
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
 setup(
-	name='nti.app.graphdb',
-	version=VERSION,
-	author='Jason Madden',
-	author_email='jason.madden@nextthought.com',
-	description="NTI App GraphDB",
-	long_description=codecs.open('README.rst', encoding='utf-8').read(),
-	license='Proprietary',
-	keywords='pyramid preference',
-	classifiers=[
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.3',
-	],
-	packages=find_packages('src'),
-	package_dir={'': 'src'},
-	namespace_packages=['nti','nti.app'],
-	install_requires=[
-		'setuptools',
-		'nti.graphdb'
-	],
-	entry_points=entry_points
+    name='nti.app.graphdb',
+    version=_read('version.txt').strip(),
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="NTI App GraphDB",
+    long_description=(_read('README.rst') + '\n\n' + _read("CHANGES.rst")),
+    license='Apache',
+    keywords=';pyramid graphdb',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    url="https://github.com/NextThought/nti.app.graphdb",
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti', 'nti.app'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'setuptools',
+        'nti.graphdb',
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+    },
+    entry_points=entry_points,
 )
