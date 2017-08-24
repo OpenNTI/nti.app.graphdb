ae239129b56473f6081761f1de13d1680b40a6be
Carlos Sanchez <papachoco@gmail.com>
2017-08-24 16:08:25 -0500

    ensure unicode; pep8

--- src/nti/app/graphdb/utils.py
+++ src/nti/app/graphdb/utils.py
@@ -4,7 +4,7 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -26,33 +26,38 @@ from nti.dataserver.users import User
 
 from nti.zodb import isBroken
 
+
 def all_user_ids(usernames=()):
-	result = set()
-	intids = component.getUtility(IIntIds)
-	for name in usernames or ():
-		user = User.get_user(name)
-		if user is not None:
-			result.add(intids.getId(user))
-	return intids.family.IF.LFSet(result)
+    result = set()
+    intids = component.getUtility(IIntIds)
+    for name in usernames or ():
+        user = User.get_user(name)
+        if user is not None:
+            result.add(intids.getId(user))
+    return intids.family.IF.LFSet(result)
+
 
 def all_cataloged_objects(users=()):
-	intids = component.getUtility(IIntIds)
-	catalog = component.getUtility(ICatalog, METADATA_CATALOG_NAME)
-	usernames = {getattr(user, 'username', user).lower() for user in users or ()}
-	if usernames:
-		intids_created_by = catalog[IX_CREATOR].apply({'any_of': usernames})
-		all_intids = intids.family.IF.union(all_user_ids(usernames), intids_created_by)
-	else:
-		all_intids = None
-		for name in (IX_CREATOR, IX_MIMETYPE, IX_CREATEDTIME):
-			ids = intids.family.IF.LFSet(catalog[name].ids())
-			if all_intids is None:
-				all_intids = ids
-			else:
-				all_intids = intids.family.IF.union(all_intids, ids)
+    intids = component.getUtility(IIntIds)
+    catalog = component.getUtility(ICatalog, METADATA_CATALOG_NAME)
+    usernames = {getattr(user, 'username', user).lower()
+                 for user in users or ()}
+    if usernames:
+        intids_created_by = catalog[IX_CREATOR].apply({'any_of': usernames})
+        all_intids = intids.family.IF.union(all_user_ids(usernames), 
+                                            intids_created_by)
+    else:
+        all_intids = None
+        for name in (IX_CREATOR, IX_MIMETYPE, IX_CREATEDTIME):
+            ids = intids.family.IF.LFSet(catalog[name].ids())
+            if all_intids is None:
+                all_intids = ids
+            else:
+                all_intids = intids.family.IF.union(all_intids, ids)
 
-	for uid in all_intids or ():
-		obj = intids.queryObject(uid)
-		if obj is None or isBroken(obj, uid) or IDeletedObjectPlaceholder.providedBy(obj):
-			continue
-		yield uid, obj
+    for uid in all_intids or ():
+        obj = intids.queryObject(uid)
+        if     obj is None or isBroken(obj, uid) \
+            or IDeletedObjectPlaceholder.providedBy(obj):
+            continue
+        yield uid, obj
--- src/nti/app/graphdb/views.py
+++ src/nti/app/graphdb/views.py
@@ -4,25 +4,24 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
 from zope import interface
 
-from zope.container.contained import Contained
-
 from zope.location.interfaces import IContained
 
 from zope.traversing.interfaces import IPathAdapter
 
+
 @interface.implementer(IPathAdapter, IContained)
-class GraphPathAdapter(Contained):
+class GraphPathAdapter(object):
 
-	__name__ = 'graphdb'
+    __name__ = 'graphdb'
 
-	def __init__(self, context, request=None):
-		self.context = context
-		self.request = request
-		self.__parent__ = context
+    def __init__(self, context, request=None):
+        self.context = context
+        self.request = request
+        self.__parent__ = context
